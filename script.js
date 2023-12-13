3e81e3fdba57f82320c9adfed1c2609ed9d170b8
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2023-12-13 18:52:38 -0500

    Add functions to open and close dialog

--- script.js
+++ script.js
@@ -18,19 +18,34 @@ function addBook() {
 addBook();
 
 // DOM Manipulation for book cards
-const cardHolder = document.querySelector('#card-holder')
+const cardHolder = document.querySelector('#card-holder');
 
+//function to display book cards
 function displayBook () {
     for(i = 0;i < newLibrary.length; i++) {
         const bookCard = document.createElement('div');
-        
-        for (let value in newLibrary[i]) {
-            console.log(newLibrary[i][value]);
-        }
-        cardHolder.appendChild(bookCard);
-    }
+        bookCard.setAttribute('class', 'book-card');
 
-    
+        for (let value in newLibrary[i]) {
+            const para = document.createElement('p');
+            para.textContent = newLibrary[i][value];
+            bookCard.appendChild(para);
+        };
+        
+        cardHolder.appendChild(bookCard);
+    };
 }
 
+//DOM Manipulation for dialog
+const dialog = document.querySelector('dialog');
+const openButton = document.querySelector('.add-button');
+const closeButton = document.querySelector('.close-button');
+
+openButton.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+closeButton.addEventListener('click', () => {
+    dialog.close();
+});
 displayBook();
