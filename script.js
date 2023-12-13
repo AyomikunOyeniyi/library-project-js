44f7ff9d8d192e92d6dfbd82025ff00e970952df
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2023-12-13 12:00:40 -0500

    Add function to display books in console

--- script.js
+++ script.js
@@ -5,18 +5,32 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-
-    this.info = function () {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
-    }
 };
 
-let userChoice;
+const theHobbit = new Book('The Hobbit','Tolkien','333','read');
+const kiteRunner = new Book('Kite Runner', 'Hosseini', '784', 'unread');
 
 function addBook() {
-   newLibrary.push(userChoice);
-   console.log(newLibrary);
+   newLibrary.push(theHobbit);
+   newLibrary.push(kiteRunner);
 };
 
-
 addBook();
+
+// DOM Manipulation for book cards
+const cardHolder = document.querySelector('#card-holder')
+
+function displayBook () {
+    for(i = 0;i < newLibrary.length; i++) {
+        const bookCard = document.createElement('div');
+        
+        for (let value in newLibrary[i]) {
+            console.log(newLibrary[i][value]);
+        }
+        cardHolder.appendChild(bookCard);
+    }
+
+    
+}
+
+displayBook();
